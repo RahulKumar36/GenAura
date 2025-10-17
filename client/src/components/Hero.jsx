2c726ebe1667685e200b952e0d05bfb044e2c959
Rahul Kumar Shyanti <rahulshyanti37@gmail.com>
2025-10-18 02:16:25 +0530

    commit changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
 
         <div className='text-center mb-6'>
-            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2] text-[#021FAB]'>Create Amazing Content <br/>With <span className='text-primary'> AI Tools</span></h1>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create Amazing Content <br/>With <span className='text-primary'> AI Tools</span></h1>
             <p className=' mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-200'>Transform your content creation with our suite of premium AI tools.Write Articles,Generate Images, and enhance your workflow.
             </p>
         </div>
